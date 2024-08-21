35da5485d2cdb7043a2f8f8a080708770716a371
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-08-21 23:01:42 +1200

    Register the models in the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Portfolio, Asset, PortfolioAsset
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Portfolio)
+admin.site.register(PortfolioAsset)
