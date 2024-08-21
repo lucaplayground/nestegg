7e876e28441639ab79f7b1f364fb11ed4e72557e
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-08-21 23:06:44 +1200

    Register models and test the models

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import User, Portfolio, Asset, PortfolioAsset
+from .models import User, Portfolio
 
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Portfolio)
-admin.site.register(PortfolioAsset)
