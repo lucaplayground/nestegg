1e6d0c1eaaccb8197902d0e813c9086f50e6717e
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-11 23:09:43 +1200

    Update urls.py
    Add URLs  for search, add, update assets

--- investments/urls.py
+++ investments/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomeView, PortfolioView
+from .views import HomeView, PortfolioView, search_assets, add_asset, update_position
 
 urlpatterns = [
     path('home/', HomeView, name='home'),
     path('portfolio/', PortfolioView, name='portfolio'),
+    path('search_assets/', search_assets, name='search_assets'),
+    path('add_asset/<int:portfolio_id>/', add_asset, name='add_asset'),
+    path('update_position/<int:portfolio_asset_id>/', update_position, name='update_position'),  # New URL pattern
 ]
