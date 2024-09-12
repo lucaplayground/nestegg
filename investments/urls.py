e301dcf2854ece14b6b4baebeff266893621ee0a
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-12 14:59:32 +1200

    Tidy up URLs, Views and add templates

--- investments/urls.py
+++ investments/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import HomeView, list_portfolios, portfolio_detail, update_portfolio, delete_portfolio, search_assets, add_asset, update_position, list_assets, asset_detail
+from .views import DashboardView, list_portfolios, add_portfolio, portfolio_detail, update_portfolio, delete_portfolio, list_portfolio_assets, search_asset, add_asset, update_position, delete_portfolio_asset
 
 urlpatterns = [
-    path('home/', HomeView, name='home'),
+    path('dashborad/', DashboardView, name='dashboard'),
     path('portfolios/', list_portfolios, name='list_portfolios'),
+    path('add_portfolio/', add_portfolio, name='add_portfolio'),
     path('portfolios/<int:portfolio_id>/', portfolio_detail, name='portfolio_detail'),
     path('portfolios/update/<int:portfolio_id>/', update_portfolio, name='update_portfolio'),
     path('portfolios/delete/<int:portfolio_id>/', delete_portfolio, name='delete_portfolio'),
-    path('assets/<int:portfolio_id>/', list_assets, name='list_assets'),
-    path('asset_detail/<int:portfolio_asset_id>/', asset_detail, name='asset_detail'),
-    path('search_assets/', search_assets, name='search_assets'),
+    path('assets/<int:portfolio_id>/', list_portfolio_assets, name='list_portfolio_assets'),
+    path('search_asset/', search_asset, name='search_asset'),
     path('add_asset/<int:portfolio_id>/', add_asset, name='add_asset'),
     path('assets/update/<int:portfolio_asset_id>/', update_position, name='update_position'),
-    path('assets/delete/<int:portfolio_id>/', delete_portfolio, name='delete_portfolio'),
+    path('assets/delete/<int:portfolio_id>/', delete_portfolio_asset, name='delete_portfolio_asset'),
 ]
