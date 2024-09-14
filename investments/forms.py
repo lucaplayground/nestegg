a10f7e629d900ec193456ef9bcd95a4ac9b5251d
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-14 17:23:11 +1200

    Edit Portfolio Form
    - Add currency field

--- investments/forms.py
+++ investments/forms.py
@@ -5,7 +5,8 @@ from .models import Portfolio
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolio
-        fields = ['name']
+        fields = ['name', 'currency']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'})
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'currency': forms.Select(attrs={'class': 'form-control'})
         }
