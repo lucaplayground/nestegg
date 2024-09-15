699bc8da7dc5d3c719297f1b7100975eebc119dc
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-15 23:18:35 +1200

    Restructure functions and methods

--- investments/models.py
+++ investments/models.py
@@ -1,28 +1,25 @@
 from django.db import models
 from django.conf import settings
-from .utils import fetch_asset_data, convert_currency
+from .constants import SUPPORTED_CURRENCY
 from decimal import Decimal
-from django.utils import timezone
 
-# This file includes all the models, their fields and methods, and all related calculations
+# This file includes models and their methods
 
 
 # Create your models here.
 class Portfolio(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='portfolios')
     name = models.CharField(max_length=100)
+    currency = models.CharField(max_length=3, choices=SUPPORTED_CURRENCY, default='USD')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
-    def get_total_value(self):
-        return sum(portfolio_asset.get_value_in_portfolio_currency() for portfolio_asset in self.portfolio_assets.all())
-        
-    def get_latest_update(self):
-        latest_asset = self.portfolio_assets.order_by('-asset__last_updated').first()
-        return latest_asset.asset.last_updated if latest_asset else self.updated_at
+
+    # def get_latest_update(self):
+    #     latest_asset = self.portfolio_assets.order_by('-asset__updated_at').first()
+    #     return latest_asset.asset.updated_at if latest_asset else self.updated_at
 
 
 class Asset(models.Model):
@@ -31,27 +28,10 @@ class Asset(models.Model):
     asset_type = models.CharField(max_length=50)  # e.g., Equity, ETF, etc.
     latest_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     currency = models.CharField(max_length=3, default='USD')  # e.g., USD, CNY, JPY, etc.
-    last_updated = models.DateTimeField(auto_now=True)  # Track when the asset data was last updated
+    updated_at = models.DateTimeField(auto_now=True)  # Track when the asset data was last updated
 
     def __str__(self):
         return f"{self.name} ({self.symbol})"
-    
-    # @classmethod
-    # def create_asset(cls, symbol, data):
-    #     return cls.objects.create(
-    #         symbol=symbol,
-    #         name=data['name'],
-    #         asset_type=data['asset_type'],
-    #         latest_price=data['latest_price'],
-    #         currency=data['currency'],
-    #         last_updated=timezone.now()
-    #     )
-    
-    # @classmethod
-    # def update_price(self, latest_price):
-    #     self.latest_price = latest_price
-    #     self.last_updated = timezone.now()
-    #     self.save()
 
 
 class PortfolioAsset(models.Model):
@@ -66,13 +46,6 @@ class PortfolioAsset(models.Model):
     def get_asset_value(self):
         asset_value = self.asset.latest_price*self.position
         return Decimal(asset_value)
-    
-    def get_asset_value_in_target_currency(self):
-        asset_value = self.get_asset_value()
-        target_currency = self.portfolio.currency
-        asset_currency = self.asset.currency
-        asset_value_in_target_currency = convert_currency(asset_value, asset_currency, target_currency)
-        return Decimal(asset_value_in_target_currency)
 
 
 class PositionHistory(models.Model):
--- investments/utils.py
+++ investments/utils.py
@@ -1,19 +1,38 @@
-from .api import get_asset_data, get_exchange_rate
 import logging
 from decimal import Decimal
 from django.utils import timezone
+from .api import get_asset_data, get_exchange_rate
+from .models import Portfolio, PortfolioAsset, Asset
 
-# This file contains all the API-related functions and general utility functions
+# This file contains general utility functions
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
-# Supported currencies in the system
-# SUPPORTED_CURRENCIES = ['USD', 'CNY', 'NZD']
 
+def create_asset(symbol):
+    """Fetch API data and create an asset in the database"""
+    try:
+        asset_data = get_asset_data(symbol)
+        if asset_data:
+            asset, created = Asset.objects.update_or_create(
+                symbol=symbol,
+                defaults={
+                    'name': asset_data['name'],
+                    'asset_type': asset_data['asset_type'],
+                    'latest_price': asset_data['latest_price'],
+                    'currency': asset_data['currency'],
+                    'updated_at': timezone.now()
+                }
+            )
 
-def add_asset_to_portfolio(portfolio, symbol, quantity):
-    pass
+        logger.info(f"Updated asset: {asset.name} ({asset.symbol})")
+        return asset
+
+    except Exception as e:
+        logger.error(f"Error updating asset data for {symbol}: {e}")
+        # print(f"Error updating asset data for {symbol}: {e}")  # Print the error for console feedback
+        return None
 
 
 def update_asset_price(asset):
@@ -22,7 +41,7 @@ def update_asset_price(asset):
         data = get_asset_data(asset.symbol)
         if data:
             asset.latest_price = data['latest_price']
-            asset.last_updated = timezone.now()
+            asset.updated_at = timezone.now()
             asset.save()
             logger.info(f"Updated latest price for asset: {asset.name} ({asset.symbol})")
             return asset
@@ -31,10 +50,44 @@ def update_asset_price(asset):
         return None
 
 
-# 
-def get_value_in_portfolio_currency(portfolio_asset):
-    value = portfolio_asset.get_value()
-    return convert_currency(value, portfolio_asset.asset.currency, portfolio_asset.portfolio.currency)
+def add_asset_to_portfolio(portfolio, symbol, quantity):
+    """Add an asset to the portfolio"""
+    try:
+        asset_data = get_asset_data(symbol)
+        if asset_data:
+            asset, _ = Asset.objects.update_or_create(
+                symbol=symbol,
+                defaults={
+                    'name': asset_data['name'],
+                    'asset_type': asset_data['asset_type'],
+                    'latest_price': asset_data['latest_price'],
+                    'currency': asset_data['currency'],
+                }
+            )
+            portfolio_asset, created = PortfolioAsset.objects.get_or_create(
+                portfolio=portfolio,
+                asset=asset,
+                defaults={'position': Decimal(quantity)}
+            )
+            if not created:
+                portfolio_asset.position += Decimal(quantity)
+                portfolio_asset.save()
+            return portfolio_asset
+    except Exception as e:
+        logger.error(f"Error adding asset {symbol} to portfolio {portfolio.name}: {e}")
+        return None
+
+
+def get_portfolio_total_value(portfolio):
+    total_value = Decimal(0)
+    for portfolio_asset in portfolio.portfolio_assets.all():
+        asset_value = portfolio_asset.get_asset_value()
+        converted_value = convert_currency(asset_value, portfolio_asset.asset.currency, portfolio_asset.portfolio.currency)
+        if converted_value:
+            total_value += converted_value
+        else:
+            logger.error(f"Error converting currency for asset: {portfolio_asset.asset.name} ({portfolio_asset.asset.symbol})")
+        return total_value
 
 
 def convert_currency(amount, from_currency, to_currency):
@@ -46,3 +99,6 @@ def convert_currency(amount, from_currency, to_currency):
         return None
     converted_amount = Decimal(amount)*exchange_rate
     return converted_amount
+
+
+
