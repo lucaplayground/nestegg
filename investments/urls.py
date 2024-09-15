ac13c1eab0caaea1a4f2fa4fa389dd7bd4dface6
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-15 17:28:15 +1200

    Update urls.py

--- investments/urls.py
+++ investments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, list_portfolios, portfolio_detail, update_portfolio, delete_portfolio, search_assets, add_asset, update_position, list_assets, asset_detail
+from .views import HomeView, list_portfolios, portfolio_detail, update_portfolio, delete_portfolio, search_assets, add_asset, update_asset_position, list_assets, asset_detail
 
 urlpatterns = [
     path('home/', HomeView, name='home'),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('asset_detail/<int:portfolio_asset_id>/', asset_detail, name='asset_detail'),
     path('search_assets/', search_assets, name='search_assets'),
     path('add_asset/<int:portfolio_id>/', add_asset, name='add_asset'),
-    path('assets/update/<int:portfolio_asset_id>/', update_position, name='update_position'),
+    path('assets/update/<int:portfolio_asset_id>/', update_asset_position, name='update_asset_position'),
     path('assets/delete/<int:portfolio_id>/', delete_portfolio, name='delete_portfolio'),
 ]
