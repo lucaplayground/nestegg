0d6c67d15d1bc165ec6e5871d7e3f005933559fe
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-15 23:18:49 +1200

    Update views.py

--- investments/views.py
+++ investments/views.py
@@ -1,34 +1,43 @@
 from django.shortcuts import render, redirect
-import yfinance as yf
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
 from .models import Asset, PortfolioAsset, PositionHistory, Portfolio
+from .utils import get_portfolio_total_value, get_asset_data, create_asset, add_asset_to_portfolio, update_asset_price
+from .forms import PortfolioForm
 
 
-# Home view
+# Dashboard view
 @login_required
-def HomeView(request):
-    return render(request, 'investments/home.html')
+def DashboardView(request):
+    return render(request, 'investments/dashboard.html')
 
 
 # List all portfolios
 @login_required
 def list_portfolios(request):
-    portfolios = Portfolio.objects.filter(user=request.user)  # Fetch portfolios for the logged-in user
+    portfolios = Portfolio.objects.filter(user=request.user).order_by('-created_at').prefetch_related('portfolio_assets__asset')
+    for portfolio in portfolios:
+        portfolio.total_value = get_portfolio_total_value(portfolio)
+        # portfolio.latest_update = portfolio.get_latest_update()
     return render(request, 'investments/portfolios.html', {'portfolios': portfolios})
 
 
-# Function to add a portfolio
+# Add a portfolio
 @login_required
 def add_portfolio(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        if name:
-            Portfolio.objects.create(user=request.user, name=name)
-            return redirect('portfolio')  # Redirect to the portfolio list page
-    return render(request, 'investments/add_portfolio.html')
+        form = PortfolioForm(request.POST)
+        if form.is_valid():
+            portfolio = form.save(commit=False)
+            portfolio.user = request.user
+            portfolio.save()
+            messages.success(request, 'Portfolio created successfully.')
+            return redirect('list_portfolios')
+    else:
+        form = PortfolioForm()
+    return render(request, 'investments/add_portfolio.html', {'form': form})
 
 
 # Portfolio detail view
@@ -38,18 +47,22 @@ def portfolio_detail(request, portfolio_id):
     return render(request, 'investments/portfolio_detail.html', {'portfolio': portfolio})
 
 
-# Function to update a portfolio
+# Edit a portfolio
 @login_required
-def update_portfolio(request, portfolio_id):
+def edit_portfolio(request, portfolio_id):
     portfolio = get_object_or_404(Portfolio, id=portfolio_id, user=request.user)
     if request.method == 'POST':
-        portfolio.name = request.POST.get('name', portfolio.name)
-        portfolio.save()
-        return redirect('portfolio_detail', portfolio_id=portfolio.id)  # Redirect to the portfolio detail page
-    return render(request, 'investments/update_portfolio.html', {'portfolio': portfolio})
+        form = PortfolioForm(request.POST, instance=portfolio)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Portfolio updated successfully.')
+            return redirect('list_portfolios')
+    else:
+        form = PortfolioForm(instance=portfolio)
+    return render(request, 'investments/edit_portfolio.html', {'form': form, 'portfolio': portfolio})
 
 
-# Function to delete a portfolio
+# Delete a portfolio
 @login_required
 def delete_portfolio(request, portfolio_id):
     portfolio = get_object_or_404(Portfolio, id=portfolio_id, user=request.user)
@@ -59,11 +72,11 @@ def delete_portfolio(request, portfolio_id):
     return render(request, 'investments/delete_portfolio.html', {'portfolio': portfolio})
 
 
-# List all assets
+# List all assets in a portfolio
 @login_required
-def list_assets(request):
-    assets = Asset.objects.all()  # Fetch all assets
-    return render(request, 'investments/list_assets.html', {'assets': assets})
+def list_portfolio_assets(request, portfolio_id):
+    assets = PortfolioAsset.objects.filter(portfolio_id=portfolio_id)  # Fetch all assets in a portfolio
+    return render(request, 'investments/portfolio_assets.html', {'assets': assets})
 
 
 # Asset detail view
@@ -73,79 +86,71 @@ def asset_detail(request, asset_id):
     return render(request, 'investments/asset_detail.html', {'asset': asset})
 
 
-# Function to search for assets
-def search_assets(request):
-    query = request.GET.get('query','')
+# Search for assets
+def search_asset(request):
+    query = request.GET.get('query', '').strip()
     if query:
-        # Fetch data from Yahoo Finance
-        ticker = yf.Ticker(query)
-        try:
-            info = ticker.info
-            asset_data = {
-                'name': info.get('longName', 'Unknown'),
-                'symbol': info.get('symbol', 'Unknown'),
-                'asset_type': info.get('quoteType', 'Unknown'),
-                'latest_price': info.get('regularMarketPrice', 0),
-                'currency': info.get('currency', 'Unknown')
-            }
-            return JsonResponse([asset_data])
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
-    return JsonResponse([], safe=False)
+        assets = Asset.objects.filter(
+            Q(symbol__icontains=query) | Q(name__icontains=query)
+        )[:10]  # Limit to 10 results for performance
+
+        results = [
+            {
+                'id': asset.symbol,
+                'text': f"{asset.symbol} - {asset.name}"
+            } for asset in assets
+        ]
+
+        # If no results found in the database, try to fetch from the API
+        if not results:
+            api_asset = create_asset(query)
+            if api_asset:
+                results.append({
+                    'id': api_asset.symbol,
+                    'text': f"{api_asset.symbol} - {api_asset.name}"
+                })
+
+        return JsonResponse({'results': results})
+    return JsonResponse({'results': []})
 
 
-# Function to add an asset to a portfolio
+# Add an asset to a portfolio
 @login_required
 def add_asset(request, portfolio_id):
     if request.method == 'POST':
         symbol = request.POST.get('symbol')
         position = request.POST.get('position', 0)
+        portfolio = get_object_or_404(Portfolio, id=portfolio_id, user=request.user)
 
-        # Fetch the asset
-        ticker = yf.Ticker(symbol)
-        info = ticker.info
-        asset, created = Asset.objects.update_or_create(
-            symbol=symbol,
-            defaults={
-                'name': info.get('longName', 'Unknown'),
-                'asset_type': info.get('quoteType', 'Unknown'),
-                'latest_price': info.get('regularMarketPrice', 0),
-                'currency': info.get('currency', 'Unknown'),
-                'last_updated': timezone.now()
-            }
-        )
+        # Call utility function to add the asset to the porffolio
+        portfolio_asset = add_asset_to_portfolio(portfolio, symbol, position)
 
-        # Add to PortfolioAsset
-        portfolio = get_object_or_404(Portfolio, id=portfolio_id)
-        portfolio_asset = PortfolioAsset.objects.create(
-            portfolio=portfolio,
-            asset=asset,
-            position=position
-        )
-
-        # Create PositionHistory entry
-        PositionHistory.objects.create(
-            portfolio_asset=portfolio_asset,
-            position=position,
-            price_at_time=asset.latest_price
-        )
+        if portfolio_asset:
+            # Create a PositionHistory entry
+            PositionHistory.objects.create(
+                portfolio_asset=portfolio_asset,
+                position=position,
+                price_at_time=portfolio_asset.asset.latest_price
+            )
 
         return JsonResponse({'success': True})
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
-# Function to update the position of an asset
+# Update the position of an asset
 @login_required
 def update_asset_position(request, portfolio_asset_id):
     if request.method == 'POST':
         new_position = request.POST.get('position', 0)
 
         # Fetch the PortfolioAsset instance
-        portfolio_asset = get_object_or_404(PortfolioAsset, id=portfolio_asset_id)
+        portfolio_asset = get_object_or_404(PortfolioAsset, id=portfolio_asset_id, portfolio__user=request.user)
 
         # Update the position
         portfolio_asset.position = new_position
         portfolio_asset.save()
+        # Update the latest price for the asset
+        update_asset_price(portfolio_asset.asset)
 
         # Create a PositionHistory entry
         PositionHistory.objects.create(
@@ -159,11 +164,11 @@ def update_asset_position(request, portfolio_asset_id):
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
-# Funciton to delete an asset
+# Delete an asset from a portfolio
 @login_required
-def delete_asset(request, asset_id):
-    asset = get_object_or_404(Asset, id=asset_id)
+def delete_portfolio_asset(request, portfolio_asset_id):
+    portfolio_asset = get_object_or_404(PortfolioAsset, id=portfolio_asset_id, portfolio_user=request.user)
     if request.method == 'POST':
-        asset.delete()
+        portfolio_asset.delete()
         return JsonResponse({'success': True})
-    return render(request, 'investments/delete_asset.html', {'asset': asset})
+    return render(request, 'investments/delete_portfolio_asset.html', {'portfolio_asset': portfolio_asset})
