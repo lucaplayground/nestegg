564ff09b3adf712d44ba8d0d5120897a206bbac1
makabaka <20857225+lucaplayground@users.noreply.github.com>
2024-09-15 23:18:44 +1200

    Update urls.py

--- investments/urls.py
+++ investments/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import HomeView, list_portfolios, portfolio_detail, update_portfolio, delete_portfolio, search_assets, add_asset, update_asset_position, list_assets, asset_detail
+from . import views
 
 urlpatterns = [
-    path('home/', HomeView, name='home'),
-    path('portfolios/', list_portfolios, name='list_portfolios'),
-    path('portfolios/<int:portfolio_id>/', portfolio_detail, name='portfolio_detail'),
-    path('portfolios/update/<int:portfolio_id>/', update_portfolio, name='update_portfolio'),
-    path('portfolios/delete/<int:portfolio_id>/', delete_portfolio, name='delete_portfolio'),
-    path('assets/<int:portfolio_id>/', list_assets, name='list_assets'),
-    path('asset_detail/<int:portfolio_asset_id>/', asset_detail, name='asset_detail'),
-    path('search_assets/', search_assets, name='search_assets'),
-    path('add_asset/<int:portfolio_id>/', add_asset, name='add_asset'),
-    path('assets/update/<int:portfolio_asset_id>/', update_asset_position, name='update_asset_position'),
-    path('assets/delete/<int:portfolio_id>/', delete_portfolio, name='delete_portfolio'),
+    path('dashboard/', views.DashboardView, name='dashboard'),
+    path('portfolios/', views.list_portfolios, name='list_portfolios'),
+    path('add_portfolio/', views.add_portfolio, name='add_portfolio'),
+    path('portfolios/<int:portfolio_id>/', views.portfolio_detail, name='portfolio_detail'),
+    path('portfolios/edit/<int:portfolio_id>/', views.edit_portfolio, name='edit_portfolio'),
+    path('portfolios/delete/<int:portfolio_id>/', views.delete_portfolio, name='delete_portfolio'),
+    path('assets/<int:portfolio_id>/', views.list_portfolio_assets, name='list_portfolio_assets'),
+    path('assets/search/', views.search_asset, name='search_asset'),
+    path('add_asset/<int:portfolio_id>/', views.add_asset, name='add_asset'),
+    path('assets/update/<int:portfolio_asset_id>/', views.update_asset_position, name='update_asset_position'),
+    path('assets/delete/<int:portfolio_id>/', views.delete_portfolio_asset, name='delete_portfolio_asset'),
 ]
